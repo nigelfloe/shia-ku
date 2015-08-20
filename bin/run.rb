53c156718451383ab300740406e43bc647ec7645
nigelfloe <dwr229@gmail.com>
2015-08-20 18:45:31 -0400

    break into syllables hash

--- bin/run.rb
+++ bin/run.rb
@@ -1,37 +1,62 @@
 require_relative '../config/environment'
 
-shia = SHIA.uniq
 
-tweet_words = shia.map do |tweet|
-  tweet.split(' ')
+# redacted = shia.delete_if do |tweet|
+#   tweet.match(/@|#|^RT|\d|http:/)
+# end
+#
+# File.open('lib/arrays/shia_redacted.rb', 'w+') {|f| f.write(p redacted)}
+#
+# binding.pry
+
+# tweet_words = shia.map do |tweet|
+#   tweet.split(' ')
+# end
+#
+#
+# redacted = tweet_words.map do |word_array|
+#   word_array.delete_if {|word| word.match(/\d|^RT$/)}
+#   word_array.each {|word| word.downcase!}
+#   # word_array.delete_if {|word| word.match(/@\w+|#\w+|http.+|^RT$|\d/)}
+# end
+#
+# redacted = redacted.reject(&:empty?)
+#
+# cleaned_tweets = redacted.map do |word_array|
+#   word_array.join(' ')
+# end
+
+
+shia = SHIA
+
+short_tweets = shia.select do |tweet|
+  tweet.to_phrase.syllables <= 17
 end
 
-redacted = tweet_words.map do |word_array|
-  word_array.delete_if {|word| word.match(/\W|\d|^RT$/)}
-  word_array.each {|word| word.downcase!}
-  # word_array.delete_if {|word| word.match(/@\w+|#\w+|http.+|^RT$|\d/)}
+syllable_sort = {}
+short_tweets.each do |tweet|
+  syllable_sort[tweet.to_phrase.syllables] ||= []
+  syllable_sort[tweet.to_phrase.syllables] << tweet
 end
-
-redacted = redacted.reject(&:empty?)
-
-cleaned_tweets = redacted.map do |word_array|
-  word_array.join(' ')
-end
-
-five_tweets = cleaned_tweets.select do |tweet|
-  tweet.to_phrase.syllables == 5
-end
-
-five_tweets.uniq!
-
-seven_tweets = cleaned_tweets.select do |tweet|
-  tweet.to_phrase.syllables == 7
-end
-
-
-
 
 binding.pry
+
+# short_tweets.uniq!
+
+
+# five_tweets = cleaned_tweets.select do |tweet|
+#   tweet.to_phrase.syllables == 5
+# end
+#
+# five_tweets.uniq!
+#
+# seven_tweets = cleaned_tweets.select do |tweet|
+#   tweet.to_phrase.syllables == 7
+# end
+
+
+
+
 
 vocabulary = words.flatten
 
